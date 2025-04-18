62e46814757dac99e5019e654bff6cd048b19178
Estevao <estevoliveira1@gmail.com>
2025-04-18 18:15:33 -0300

    created varible imgUrl

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -9,18 +9,21 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity,String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
     public OrderItemDTO(OrderItem orderItem) {
         this.productId = orderItem.getProduct().getId();
         this.name = orderItem.getProduct().getName();
         this.price = orderItem.getPrice();
         this.quantity = orderItem.getQuantity();
+        this.imgUrl = orderItem.getProduct().getImgUrl();
     }
 
     public OrderItemDTO() {
@@ -44,5 +47,9 @@ public class OrderItemDTO {
 
     public Double getSubTotal(){
         return price * quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 }
